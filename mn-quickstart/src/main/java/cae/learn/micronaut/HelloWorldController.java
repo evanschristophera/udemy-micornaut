b59c8c201d5cfb1ffd8f26c4289932ae9ef73b9d
Christopher Evans <chris.evans@quantitech.com>
2022-03-28 16:15:53 -0500

    Now I know all about logging.

--- mn-quickstart/src/main/java/cae/learn/micronaut/HelloWorldController.java
+++ mn-quickstart/src/main/java/cae/learn/micronaut/HelloWorldController.java
@@ -3,9 +3,12 @@ package cae.learn.micronaut;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Controller("/hello")
 public class HelloWorldController {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
     private final MyService service;
 
@@ -18,6 +21,7 @@ public class HelloWorldController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld(){
+        log.debug("Called the " + this.getClass().getName() );
         return service.helloFromService();
     }
 }
