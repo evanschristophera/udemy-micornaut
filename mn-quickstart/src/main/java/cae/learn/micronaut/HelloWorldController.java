c1a6ec690e8495dd9b4f66973d2e7146c1bc8457
Christopher Evans <chris.evans@quantitech.com>
2022-03-28 17:16:43 -0500

    Injection from the application.yml file!  Cool!

--- mn-quickstart/src/main/java/cae/learn/micronaut/HelloWorldController.java
+++ mn-quickstart/src/main/java/cae/learn/micronaut/HelloWorldController.java
@@ -1,5 +1,6 @@
 package cae.learn.micronaut;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -12,9 +13,11 @@ public class HelloWorldController {
 
     private final MyService service;
 
+    private final String helloFromConfig;
     //Constructor based injection
-    public HelloWorldController( MyService service) {
+    public HelloWorldController(MyService service, @Property( name = "hello.world.message") String helloFromConfig) {
         this.service = service;
+        this.helloFromConfig = helloFromConfig;
     }
 
 
@@ -24,4 +27,11 @@ public class HelloWorldController {
         log.debug("Called the " + this.getClass().getName() );
         return service.helloFromService();
     }
+
+    @Get(uri ="/config", produces = MediaType.TEXT_PLAIN)
+    public String helloConfig(){
+        log.debug("Return value from application.yml");
+        return helloFromConfig;
+    }
+
 }
