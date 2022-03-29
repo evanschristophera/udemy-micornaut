e0ad01f616902bac335b26ae14b98bec5d05a97a
Christopher Evans <chris.evans@quantitech.com>
2022-03-29 14:19:49 -0500

    Query parameters are used to filter or search for specific data

--- mn-stock-broker/src/main/java/com/evans/chris/stockbroker/broker/SymbolsController.java
+++ mn-stock-broker/src/main/java/com/evans/chris/stockbroker/broker/SymbolsController.java
@@ -4,11 +4,14 @@ import com.evans.chris.stockbroker.broker.data.InMemoryStore;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller("/symbols")
 public class SymbolsController {
@@ -30,4 +33,15 @@ public class SymbolsController {
     public Symbol getSymbolByValue(@PathVariable String value ){ // the name used here
         return inMemoryStore.getSymbols().get(value);
     }
+
+    @Get("/filter{?max,offset}")
+    public List<Symbol> getSymbols(@QueryValue Optional <Integer> max, @QueryValue Optional<Integer> offset ){
+        return inMemoryStore.getSymbols().values()
+                .stream()
+                .skip(offset.orElse(0))
+                .limit(max.orElse(0))
+                .toList();
+
+    }
+
 }
